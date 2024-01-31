06b37269c7569880f9dce7bc320800025195460d
Adrtris <adrtris@student.ubc.ca>
2024-01-30 17:48:01 -0800

    tutorial assets (w/o animation sheets)

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,11 +1,19 @@
 extends CharacterBody2D
 
-@export var speed = 400
+@export var speed: int = 400
+var mode: int = 0:
+	set(value):
+		mode = value
+	get:
+		return mode
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func get_input():
-	var input_direction = Input.get_vector("left", "right", "up", "down")
+	var input_direction = Input.get_vector("a", "d", "w", "s")
 	velocity = input_direction * speed
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	get_input()
 	move_and_slide()
+	

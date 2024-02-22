2b5df52f89009e427174eab0c4f17d494eefca65
DANNY\danhn <danhngo80@gmail.com>
2024-02-22 18:50:08 -0500

    Modified the platform colliders
    - One way collision to allow our player to move through the platform
    - Increased height of player detector collider so player lands on the platform when failling off another platform
    - Decreased player collider size

--- scripts/platforms.gd
+++ scripts/platforms.gd
@@ -2,10 +2,6 @@ extends StaticBody2D
 
 var player_on_platform: bool = false
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
 func _process(delta):
 	if Input.is_action_pressed("s") and player_on_platform:
 		set_collision_layer_value(2, false)
@@ -13,11 +9,10 @@ func _process(delta):
 func _on_area_2d_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
 	if (body.name == "player"):
 		player_on_platform = true
-		print("Player is on platform")
+		set_collision_layer_value(2, true)
 
 
 func _on_area_2d_body_shape_exited(body_rid, body, body_shape_index, local_shape_index):
 	if (body.name == "player"):
 		player_on_platform = false
-		set_collision_layer_value(2, true)
-		print("Player is not on platform")
+		set_collision_layer_value(2, false)

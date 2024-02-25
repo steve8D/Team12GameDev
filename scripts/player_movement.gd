c1e4bf4fa86df0e5c29839e4e52bbbc249efeade
DANNY\danhn <danhngo80@gmail.com>
2024-02-25 18:36:47 -0500

    Reimplemented sprint without using timer node, and with stamina as a cost

--- scripts/player_movement.gd
+++ scripts/player_movement.gd
@@ -1,16 +1,44 @@
 extends CharacterBody2D
 
 const SPEED = 200.0
-const SPRINT_SPEED = 400.0
 const JUMP_VELOCITY = -600.0
 
+const SPRINT_SPEED = 400.0
+const RUNNING_STAMINA_COST = 50
+const STAMINA_RECOVERY_RATE = 100
+
+var stamina = 100
 var sprinting = false
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-@onready
-var sprintTimer = $SprintTimer
-
 func _physics_process(delta):
+	var direction = Input.get_axis("move_left", "move_right")
+	handle_vertical_movement(delta)
+	handle_lateral_movement(delta)
+	check_for_sprint()	
+	handle_sprint(direction, delta)
+	move_and_slide()
+	
+func _on_sprint_timer_timeout():
+	sprinting = false
+
+# Helper functions
+func check_for_sprint():
+	if Input.is_action_just_pressed("sprint") and is_on_floor() and stamina >= 0:
+		sprinting = true
+	
+	if Input.is_action_just_released("sprint") or stamina <= 0:
+		sprinting = false
+
+func handle_sprint(direction, delta):
+	if sprinting:
+		velocity.x = direction * SPRINT_SPEED
+		stamina -= delta * RUNNING_STAMINA_COST
+	else:
+		if stamina <= 100:
+			stamina += delta * STAMINA_RECOVERY_RATE
+
+func handle_vertical_movement(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -21,29 +49,11 @@ func _physics_process(delta):
 	# Handle drop from platform
 	if Input.is_action_pressed("jump_down") and not is_on_floor():
 		velocity.y = -JUMP_VELOCITY
-		
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+
+func handle_lateral_movement(delta):
 	var direction = Input.get_axis("move_left", "move_right")
 
 	if direction:
 		velocity.x = direction * SPEED	
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-	
-	if sprinting:
-		velocity.x = direction * SPRINT_SPEED
-	
-	check_for_sprint()
-	move_and_slide()
-	
-func _on_sprint_timer_timeout():
-	sprinting = false
-	
-func check_for_sprint():
-	if Input.is_action_just_pressed("sprint") and is_on_floor():
-		sprinting = true
-		sprintTimer.start()
-	
-	if Input.is_action_just_released("sprint"):
-		sprinting = false
